5f7b03b510437e8bf21710f87422580a9c3c92e6
maru44 <takumaru22244@gmail.com>
2021-09-04 07:15:37 +0900

    first cursor

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,34 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+import path = require('path');
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+let getPath = (args: any) => {
+	let path = null;
+	if (args && args.length > 0) {
+		path = args[0].fsPath;
+	}
+	if (!path) path = vscode.window.activeTextEditor?.document.fileName;
+	return path;
+}
+
 export function activate(context: vscode.ExtensionContext) {
 	
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "cursor" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('cursor.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from cursor!');
+	let disposable = vscode.commands.registerCommand('cursor', (...args) => {
+		var fullPath = getPath(args);
+		console.log(args);
+		console.log(typeof args);
+		var extName = path.extname(fullPath);
+        var fileName = path.basename(fullPath, extName);
+		vscode.window.setStatusBarMessage(fileName, 2000);
 	});
 
 	context.subscriptions.push(disposable);
+	
 }
 
 // this method is called when your extension is deactivated
