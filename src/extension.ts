5408b61ca4338932d833a77b0965a0efef691641
maru44 <takumaru22244@gmail.com>
2021-09-05 07:00:38 +0900

    WIP: cursor

--- src/extension.ts
+++ src/extension.ts
@@ -12,6 +12,30 @@ let getPath = (args: any) => {
 	return path;
 }
 
+class SampleHoverProvider {
+	provideHover(document: any, position: any, token: any) {
+		console.log("hovering!!");
+		let wordRange = document.getWordRangeAtPosition(position);
+		if (wordRange) {
+			let text = new vscode.MarkdownString("aaaaaaa");
+			if (!text) {
+				return Promise.reject("unmatched");
+			}
+			return Promise.resolve(new vscode.Hover(text));
+		}
+	}
+}
+
+class SampleHoverProvider2 {
+	provideHover(document: any, position: any, token: any) {
+		let wordRange = document.getWordRangeAtPosition(position, /[a-zA-Z0-9_.\[\]]+/);
+        if (wordRange === undefined) return Promise.reject("no word here");
+
+        let currentWord = document.lineAt(position.line).text.slice(wordRange.start.character, wordRange.end.character);
+        return Promise.resolve(new vscode.Hover(currentWord));
+	}
+}
+
 export function activate(context: vscode.ExtensionContext) {
 	
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
@@ -20,18 +44,16 @@ export function activate(context: vscode.ExtensionContext) {
 
 	let disposable = vscode.commands.registerCommand('cursor.cursor', (...args) => {
 		var fullPath = getPath(args);
-		console.log(args);
-		console.log(typeof args);
 		var extName = path.extname(fullPath);
         var fileName = path.basename(fullPath, extName);
 		// vscode.window.setStatusBarMessage(fileName, 2000);
-		Promise.resolve(new vscode.Hover("aaaaaaa"));
+		// Promise.resolve(new vscode.Hover(fileName));
+		console.log(fullPath);
 	});
 
 	context.subscriptions.push(disposable);
 
 	/*   sample   */
-
 	disposable = vscode.commands.registerCommand('cursor.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
@@ -39,6 +61,11 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 
 	context.subscriptions.push(disposable);
+
+	/*   hover   */
+	// context.subscriptions.push(vscode.languages.registerHoverProvider('cursor.hover', new SampleHoverProvider));
+	context.subscriptions.push(vscode.languages.registerHoverProvider('cursor.hover', new SampleHoverProvider2));
+	context.subscriptions.push(vscode.languages.registerHoverProvider({scheme: 'file', language: 'ts'}, new SampleHoverProvider2));
 }
 
 // this method is called when your extension is deactivated
