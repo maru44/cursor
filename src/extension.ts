9e1c3da3a67498d8075ccf65fb39580365145e1a
maru44 <takumaru22244@gmail.com>
2021-09-11 23:14:08 +0900

    straying

--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,6 @@
 // Import the module and reference it with the alias vscode in your code below
 import path = require('path');
 import * as vscode from 'vscode';
-import { register } from './commands';
 
 let getPath = (args: any) => {
 	let path = null;
@@ -70,11 +69,8 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(disposable);
 
 	/*   hover   */
-	// context.subscriptions.push(vscode.languages.registerHoverProvider('cursor.hover', new SampleHoverProvider));
 	context.subscriptions.push(vscode.languages.registerHoverProvider("*", new SampleHoverProvider2));
-	// context.subscriptions.push(vscode.languages.registerHoverProvider("*", new SampleHoverProvider3));
 
-	register();
 }
 
 // this method is called when your extension is deactivated
--- src/helpers/fileconfig.ts
+++ src/helpers/fileconfig.ts
@@ -1,15 +1,42 @@
 import * as vscode from 'vscode';
 
 /** Get configuration of vs code. */
-export const getConfig = (section?: string) => {
-  return vscode.workspace.getConfiguration(section);
-};
+// export const getConfig = (section?: string) => {
+//   return vscode.workspace.getConfiguration(section);
+// };
   
 /** Get list of configuration entries of package.json */
-export const getConfigProperties = (): { [config: string]: any } => {
-  return vscode.extensions.getExtension('maru44.cursor')!.packageJSON
-    .contributes.configuration.properties;
-};
+// export const getConfigProperties = (): { [config: string]: any } => {
+//   return vscode.extensions.getExtension('maru44.cursor')!.packageJSON
+//     .contributes.configuration.properties;
+// };
+
+export const performPatch = () => {
+  const config = vscode.workspace.getConfiguration("maru44.cursor");
+  if (!isProperlyConfigured(config)) {
+    return vscode.window.showInformationMessage("not configured")
+  }
+  console.log(config);
+}
+
+const patchHtml = async (config: any) => {
+  console.log(config);
+  let res = "";
+  for (const item of config.imports) {
+    const imp = await patchHtmlForItem(item);
+    if (imp) res += imp;
+  }
+}
+
+const patchHtmlForItem = (url: string) => {
+  if (!url) return "";
+  if (typeof url !== "string") return "";
+
+}
+
+const isProperlyConfigured = (config: any) => {
+  return config && config.imports && config.imports instanceof Array;
+}
 
 // <div class="monaco-list-row" role="treeitem" data-index="13" data-last-element="false" aria-setsize="6" aria-posinset="5" id="list_id_1_13" aria-selected="false" aria-label="s3handler.go" aria-level="4" draggable="true" style="top: 286px; height: 22px; line-height: 22px;">
 //   <div class="monaco-tl-row">
