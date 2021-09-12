240b0424b807d56c325d6416530c8bbfcc58efe2
maru44 <takumaru22244@gmail.com>
2021-09-13 07:39:27 +0900

    wip err not loaded

--- src/css.ts
+++ src/css.ts
@@ -1,7 +1,11 @@
 import * as vscode from 'vscode';
 
 export const importExtension = (ext: vscode.Extension<any>) => {
-    console.log(ext);
     const importedExtension = ext.exports;
     return importedExtension;
 };
+
+export const activeExtension = async (ext: vscode.Extension<any>) => {
+    await ext.activate();
+    return 
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,29 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import { importExtension } from './css';
+import { activeExtension, importExtension } from './css';
 
 const cssExt = vscode.extensions.getExtension('be5invis.vscode-custom-css');
+// occured
+// Cannot activate the 'cursor' extension because it depends on the 'Custom CSS and JS Loader' extension, which is not loaded. Would you like to reload the window to load the extension?
 
-export function activate(context: vscode.ExtensionContext) {
-	const cssExports = importExtension(cssExt!);
-
-	vscode.commands.executeCommand(cssExports.installCssCustomCSS);
+export async function activate(context: vscode.ExtensionContext) {
+	console.log("a")
+	// await activeExtension(cssExt!);
+	// console.log("b")
+	// const cssExports = importExtension(cssExt!);
+	// console.log("c");
+	// vscode.commands.executeCommand(cssExports.installCssCustomCSS);
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
+
+// const initCursor = async () => {
+// 	console.log("a")
+// 	await activeExtension(cssExt!);
+// 	console.log("b")
+// 	const cssExports = importExtension(cssExt!);
+// 	console.log("c");
+// 	vscode.commands.executeCommand(cssExports.installCssCustomCSS);
+// }
