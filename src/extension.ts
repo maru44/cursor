2381691331d43084b380c2ae35da0cb4723631d2
maru44 <takumaru22244@gmail.com>
2021-09-13 08:25:40 +0900

    wip err not loaded

--- src/extension.ts
+++ src/extension.ts
@@ -4,11 +4,11 @@ import * as vscode from 'vscode';
 import { activeExtension, importExtension } from './css';
 
 const cssExt = vscode.extensions.getExtension('be5invis.vscode-custom-css');
-// occured
+// occured here or package.json's extensionsDependencies
 // Cannot activate the 'cursor' extension because it depends on the 'Custom CSS and JS Loader' extension, which is not loaded. Would you like to reload the window to load the extension?
 
-export async function activate(context: vscode.ExtensionContext) {
-	console.log("a")
+export function activate(context: vscode.ExtensionContext) {
+	console.log("a");
 	// await activeExtension(cssExt!);
 	// console.log("b")
 	// const cssExports = importExtension(cssExt!);
